b8c84ab2b507261e4f8f0384bf58cc26b53342c5
Michael Iverson <ivers523@umn.edu>
2019-11-26 20:03:45 -0600

    connected site to api

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,24 @@
-const apiKey = "8f77f345ff105aec1b870529690d48f3";
-const cityName = documnent.get
-const queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + 
+const apiKey = "7a5f9d8af7411b1dde7d32647376929f";
+let cityName = "Minneapolis"
 
-// get query url, axious function
+// get query url, axious function to ensure queryurl is functioning
+    // function getURL() {
+    //     const queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID="+apiKey;
+    //     axios.get(queryURL)
+    //         .then(function (response) {
+    //             console.log(response)
 
-axios.get(queryURL)
-    .then(function (response) {
-        console.log("response", response);
+    //         })
+    //     }
+    // getURL();
 
+    function citySearch (){
+        const cityURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+ "&units=imperial&appid=" +apiKey;
 
-    });
-
+        axios.get(cityURL)
+            .then(function (response) {
+                console.log(response)
+                document.getElementById("weather-jumbo").setAttribute.innerHTML = [data.data.name]
+            })
+    };
+    citySearch ();
